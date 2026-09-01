b71cd1cb9b10458d75e773add3c69e80da39064b
gradientspaceai <gradientspaceai@users.noreply.github.com>
2026-09-01 09:06:21 -0400

    Update manifest and index after B77 merge

--- src/index.ts
+++ src/index.ts
@@ -265,6 +265,7 @@ export * from './Matrix';
 export * from './Matrix2x2';
 export * from './Matrix3x3';
 export * from './Matrix4x4';
+export * from './Mesh';
 export * from './MeshSmoother';
 export * from './MeshStaticManifold2';
 export * from './MeshStaticManifold3';
